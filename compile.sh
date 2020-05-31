867cf35c8aaf280f10fe11f78066c89e928af2c8
Sebastien Bouquet <bouquet.sebastien@gmail.com>
2020-05-31 21:59:53 +0200

    modified the name of the file to compile in compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 # Compilation
-clang++ -std=c++11 -Wall -Werror -g -c -I/opt/local/include -I/opt/local/include/eigen3 test.cpp -o test.o
+clang++ -std=c++11 -Wall -g -c -I/opt/local/include -I/opt/local/include/eigen3 path_planner.cpp -o path_planner.o
 
 # Linking
-clang++ -L/opt/local/lib -lompl -lompl_app -lompl_app_base test.o -o test
+clang++ -L/opt/local/lib -lompl -lompl_app -lompl_app_base path_planner.o -o path_planner
